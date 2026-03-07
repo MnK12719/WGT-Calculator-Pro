bee7a1c81a128f8b1867271e8eea4e2db7e9ce25
MnK12719 <mnk12719@gmail.com>
2026-03-07 16:45:31 -0600

    Update electron-main.js

--- electron-main.js
+++ electron-main.js
@@ -1,1 +1,13 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: { nodeIntegration: true, contextIsolation: false }
+  });
+  win.loadFile(path.join(__dirname, 'dist/index.html'));
+}
+
+app.on('ready', createWindow);
